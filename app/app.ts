13fa671fcb02155650ef4456784e80fd0f197cdd
Edward Bebbington <47337480+ebebbington@users.noreply.github.com>
2021-08-06 01:23:43 +0100

    add tests (#6)
    * add tests
    
    * fix tests
    
    * fix tests

--- app/app.ts
+++ app/app.ts
@@ -16,37 +16,43 @@ class Res extends Drash.Http.Resource {
     const url =
       decodeURIComponent(this.request.getBodyParam("url") as string) ?? "";
     const increaseBy = this.request.getBodyParam("increaseBy") ?? "1.5dB";
-    const title = await getTitleOfVideoFromUrl(url);
-    const stream = await ytdl(url);
-    const chunks: Uint8Array[] = [];
-    for await (const chunk of stream) {
-      chunks.push(chunk);
+    try {
+      const title = await getTitleOfVideoFromUrl(url);
+      const stream = await ytdl(url);
+      const chunks: Uint8Array[] = [];
+      for await (const chunk of stream) {
+        chunks.push(chunk);
+      }
+      const blob = new Blob(chunks);
+      const mp4filename = title + ".mp4";
+      Deno.writeFileSync(
+        mp4filename,
+        new Uint8Array(await blob.arrayBuffer()),
+      );
+      const videoRender = ffmpeg({ input: mp4filename, ffmpegDir });
+      const mp3filename = title + ".mp3";
+      const mp3 = await videoRender
+        .noVideo() // ignore video
+        .audioCodec("libmp3lame") // to reduce filesize
+        .audioBitrate(100) // to reduce filesize
+        .audioFilters({
+          filterName: "volume=" + increaseBy,
+        })
+        .save("pipe:1", false, { f: "mp3" });
+      Deno.removeSync(mp4filename);
+      this.response.headers.set("Content-Length", String(mp3.length));
+      this.response.headers.set("Content-Type", "application/mpeg");
+      this.response.headers.set(
+        "Content-Disposition",
+        "attachment; filename=" + mp3filename,
+      );
+      this.response.body = mp3;
+      return this.response;
+    } catch (_e) {
+      this.response.status_code = 500;
+      this.response.body = "";
+      return this.response;
     }
-    const blob = new Blob(chunks);
-    const mp4filename = title + ".mp4";
-    Deno.writeFileSync(
-      mp4filename,
-      new Uint8Array(await blob.arrayBuffer()),
-    );
-    const videoRender = ffmpeg({ input: mp4filename, ffmpegDir });
-    const mp3filename = title + ".mp3";
-    const mp3 = await videoRender
-      .noVideo() // ignore video
-      .audioCodec("libmp3lame") // to reduce filesize
-      .audioBitrate(100) // to reduce filesize
-      .audioFilters({
-        filterName: "volume=" + increaseBy,
-      })
-      .save("pipe:1", false, { f: "mp3" });
-    Deno.removeSync(mp4filename);
-    this.response.headers.set("Content-Length", String(mp3.length));
-    this.response.headers.set("Content-Type", "application/mpeg");
-    this.response.headers.set(
-      "Content-Disposition",
-      "attachment; filename=" + mp3filename,
-    );
-    this.response.body = mp3;
-    return this.response;
   }
 }
 const server = new Drash.Http.Server({
