bd039837795e4ab313d6309309236174380b3883
ebebbington <ebebbington@users.noreply.github.com>
2022-01-21 00:09:12 +0000

    chore: Update dependencies

--- cli/deps.ts
+++ cli/deps.ts
@@ -1,3 +1,3 @@
-export { Drash } from "https://deno.land/x/drash@v2.3.0/mod.ts";
+export { Drash } from "https://deno.land/x/drash@v2.4.0/mod.ts";
 export { ytdl } from "https://deno.land/x/ytdl_core@v0.0.2/src/video.ts";
 export { ffmpeg } from "https://deno.land/x/deno_ffmpeg@v3.1.0/src/ffmpegClass.ts";
